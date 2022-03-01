056c50aa131e1b874c6bd1890fbfa19aa07313fd
Javier Sanchez-Bonilla <javier.sanchezbonilla.n@gmail.com>
2022-03-01 15:57:35 +0000

    final tests for 2e and 2f

--- test_flood.py
+++ test_flood.py
@@ -1,5 +1,9 @@
-from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level, plot_water_levels
+from floodsystem.flood import plot_water_level_with_fit
 from data_test import build_test_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
 
 
 def test_stations_level_over_threshold():
@@ -29,3 +33,29 @@ def test_stations_highest_rel_level():
     assert top_three_test[0].name == 'Hexham Wydon Water'
     assert top_three_test[1].name == 'Coniston Church Bridge'
     assert top_three_test[2].name == 'Brampton Moat Side'
+
+
+def test_plot_water_levels():
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    stations_at_risk = stations_highest_rel_level(stations, 2)
+    for station in stations_at_risk[1:]:
+        dt = 10
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
+
+
+def test_plot_water_levels_with_fit():
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    stations_at_risk = stations_highest_rel_level(stations, 2)
+    for station in stations_at_risk[1:]:
+        dt = 2
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_level_with_fit(station, dates, levels, 4)

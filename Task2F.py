c5fdf549c608d8b4e4056c4120b9137d1fecbef0
Javier Sanchez-Bonilla <javier.sanchezbonilla.n@gmail.com>
2022-03-01 15:42:30 +0000

    task2f done

--- Task2F.py
+++ Task2F.py
@@ -1,11 +1,11 @@
-from floodsystem.flood import stations_highest_rel_level, plot_water_levels
+from floodsystem.flood import stations_highest_rel_level, plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 
 
 def run():
-    """Run Task 2E deliverable"""
+    """Run Task 2F deliverable"""
     stations = build_station_list()
 
     # Update latest level data for all stations
@@ -13,11 +13,11 @@ def run():
 
     stations_at_risk = stations_highest_rel_level(stations, 6)
     for station in stations_at_risk[1:]:
-        dt = 10
+        dt = 2
         dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_levels(station, dates, levels)
+        plot_water_level_with_fit(station, dates, levels, 4)
 
 
 if __name__ == "__main__":
-    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
     run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,8 @@
 from .utils import sorted_by_key
 import matplotlib.pyplot as plt
 import numpy as np
+from .analysis import polyfit
+import matplotlib
 
 
 def stations_level_over_threshold(stations, tol):
@@ -45,6 +47,31 @@ def plot_water_levels(station, dates, levels):
     plt.xticks(rotation=45)
     plt.title(station.name)
     plt.legend(handles=[line_low, line_high])
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
+
+
+def plot_water_level_with_fit(station, dates, levels, p):
+    """Plots the water level at different dates and a best fit polynomial for a given station."""
+    # Plot data
+    plt.plot(dates, levels)
+    line_high, = plt.plot(dates, np.full(len(dates), station.typical_range[1]), 'b--', label='Typical high level')
+    line_low, = plt.plot(dates, np.full(len(dates), station.typical_range[0]), 'r--', label='Typical low level')
+
+    # Construct and plot polynomial
+    poly, d0 = polyfit(dates, levels, p)
+
+    poly_line, = plt.plot(dates, poly(matplotlib.dates.date2num(dates) - d0), 'g', label=f'Polynomial of degree {p}')
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('Date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45)
+    plt.title(station.name)
+    plt.legend(handles=[poly_line, line_high, line_low])
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
